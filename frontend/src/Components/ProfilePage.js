59e60d9335c2f1abf21b18ee5ecc8a10f39fbffc
bryantriana <bryantrianarueda@gmail.com>
2022-04-01 10:26:26 -0400

    Add fetch statement for profile page

--- frontend/src/Components/ProfilePage.js
+++ frontend/src/Components/ProfilePage.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import axios from "axios";
 
 function ProfilePage() {
+  // Note to frontend: in the request below you should add the uid as
+  // a parameter in place of the "0". The request returns the profile
+  // information related to the user with the given uid.
+  useEffect(() => {
+    axios.get("http://localhost:3500/user/0")
+      .then((res) => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }, []);
+
   return (
     <div>ProfilePage</div>
   )

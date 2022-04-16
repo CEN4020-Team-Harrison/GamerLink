d8b6ad22f6ac80e348d3f7fa3f3183086884bb67
Dan Nguyen <danlily231@gmail.com>
2022-04-16 00:57:37 -0400

    nits

--- frontend/src/Components/GamePage.js
+++ frontend/src/Components/GamePage.js
@@ -97,32 +97,34 @@ const getGamesCallback = (gid, setReplies) => {
 };
 
 const ReplyItem = ({ reply }) => {
-  const time = unixTimeConvert(reply.timestamp);
+  var time = new Date(reply.timestamp);
 
   return (
     <div className="bg-white rounded-lg shadow-md border-solid border-2 w-8/12 p-2 mb-2 border-gray-300">
-      <a href={reply.username ? `/profile/:${reply.username}` : null}>
-        {reply.username ? (
-          <div className="flex items-center">
-            <span className="font-semibold mr-2">{reply.username}</span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5 text-green-700"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </div>
-        ) : (
-          <span className="font-semibold mr-3">Anonymous</span>
-        )}
-        <span className="text-gray-500 text-sm">{time}</span>
-      </a>
+      <div className="flex items-center">
+        <a href={reply.username ? `/profile/:${reply.username}` : null}>
+          {reply.username ? (
+            <div className="flex items-center">
+              <span className="font-semibold mr-1">{reply.username}</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5 text-green-700"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
+          ) : (
+            <span className="font-semibold">Anonymous</span>
+          )}
+        </a>
+        <span className="ml-2 text-gray-500 text-sm">{time.toLocaleString()}</span>
+      </div>
       <div className="pt-1">
         <p className="">{reply.message}</p>
       </div>
